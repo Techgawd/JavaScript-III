699d3483862164722d37e23a52a6a49a1cba22be
Techgawd <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2019-04-23 19:30:33 -0400

    Principles of this explanations and one example complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - JS doesn't know exactly what you're refering to so it applies on a wider scale. The least specific use of the "this" keyword. 
+* 2. Implicit Binding - For use with objects and methods. Points directly to the method that is called. Very straight-forward and specific. 
+* 3. Explicit Binding - For use with functions to access outside ojects. Use call or apply keywords to invoke.
+* 4. Bind - Saves the "this" keyword for later using a function. 
 *
 * write out a code example of each explanation above
 */
@@ -16,7 +16,14 @@
 // Principle 2
 
 // code example for Implicit Binding
-
+const person = {
+    "name": "Ben",
+    "coffee": "Machiatto",
+    "order": function(){
+        return `${this.name} always orders the ${this.coffee}`;
+    } 
+}
+console.log (person.order());
 // Principle 3
 
 // code example for New Binding
